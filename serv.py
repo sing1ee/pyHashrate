eb2eb5cc2a0397d85dc48c168692d7957aabcffc
zc <zh.milo@gmail.com>
2017-11-14 17:53:30 +0800

    add json api

--- serv.py
+++ serv.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from flask import render_template
+from flask import render_template, jsonify
 from config import app, db
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate,MigrateCommand
 from model import query_last_n_days
 from itertools import groupby
+import simplejson as json
+import collections
 import sys
 
 
@@ -22,25 +24,39 @@ manager.add_command('db',MigrateCommand)
 @app.route('/HashChart')
 def hash():
     ret = query_last_n_days(3)
-    ret_list = [(name, list(group)) for name, group in groupby(ret, lambda p:p.relayed_by)]
-    all_legends = map(lambda x: x[0], ret_list)
-    legends = []
-    filt = set()
-    for l in all_legends:
-        if l in filt:
-            continue
-        legends.append(l)
-        filt.add(l)
+    group_all = collections.defaultdict(list)
+    for hr in ret:
+        group_all[hr.relayed_by].append(hr)
+    legends = group_all.keys()
     series = []
 
     x_axis = []
-    for (key, group) in ret_list:
-        sorted(group, lambda x: x.created_at)
+    for key in legends:
+        group = group_all[key]
+        sorted(group, key = lambda x: x.created_at)
         x_axis = map(lambda x: x.created_at, group)
         series.append({'name': key, 'type': 'line', 'stack': '算力', 'data': map(lambda x: x.hashrate, group)})
 
-    return render_template('hash.html', legends=legends, series=series, x_axis=x_axis)
+    return render_template('hash.html', legends=json.dumps(legends, ensure_ascii=True).replace('"', '\''), series=series, x_axis=x_axis)
 
+
+@app.route('/data_json')
+def data_json():
+    ret = query_last_n_days(3)
+    group_all = collections.defaultdict(list)
+    for hr in ret:
+        group_all[hr.relayed_by].append(hr)
+    legends = group_all.keys()
+    series = []
+
+    x_axis = []
+    for key in legends:
+        group = group_all[key]
+        sorted(group, key = lambda x: x.created_at)
+        x_axis = map(lambda x: x.created_at, group)
+        series.append({'name': key, 'type': 'line', 'stack': '算力', 'data': map(lambda x: x.hashrate, group)})
+
+    return jsonify({"legends": legends, "series": series, "x_axis": x_axis})
 
 if __name__=='__main__':
     manager.run()
